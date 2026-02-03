c92834e1b776fbf90f7150f0e01dc03136b6b0a5
alaametawh <183715521+alaametawh@users.noreply.github.com>
2026-02-03 04:19:08 +0200

    feat: finish hero & responsitivity

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,21 @@
-const toggle_btn = document.querySelector(".navcircle.Toggle-theme")
-
 // Toggle theme button
+const toggle_btn = document.querySelector(".navcircle.Toggle-theme")
 toggle_btn.addEventListener("click", () => {
     theme = document.documentElement.getAttribute("data-theme") === "dark"
             ? "light"
             : "dark";
     document.documentElement.setAttribute("data-theme", theme);
 })
+
+// Typing effect
+const typed = new Typed('.typed', {
+  strings: [
+    "Web Developer",
+    "CS Student",
+  ],
+  typeSpeed: 40,
+  backSpeed: 50,
+  startDelay: 500,
+  backDelay: 1500,
+  loop: true
+});
